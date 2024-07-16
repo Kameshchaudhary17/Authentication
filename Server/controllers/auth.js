2fc53738cd118b8b7031a9469c9cddd76db22d86
Kamesh Chaudhary <kameshchaudhary17@gmail.com>
2024-07-16 09:07:49 +0545

    crud

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -82,3 +82,51 @@ export const registerUser = async (req, res) => {
     return sendResponse(res, 500, "Internal server error");
   }
 };
+
+export const createBlog = (req,res)=>{
+
+  const sql = "INSERT into blog (`title`,`description`) VALUE (?,?)";
+
+  const value = req.body;
+
+  db.query(sql,[value.title, value.description],(err,result)=>{
+    if(err) return res.status(500).send(err);
+    return res.status(200).send({message: "value inserted...", result})
+  })
+}
+
+export const getBlog = (req, res)=>{
+  const sql = "SELECT * from blog"
+
+  db.query(sql,(err,result)=>{
+      if(err) return res.status(500).send(err);
+      return res.status(200).send({message:"get details", result})
+  });
+};
+
+
+export const deleteBlog = (req,res)=>{
+
+  const {id} = req.params;
+
+  const sql = "DELETE from blog WHERE id = ?";
+
+  db.query(sql,id,(err,result)=>{
+      if(err) return res.status(500).send(err);
+      return res.status(200).send({message: "value deleted", result})
+  })
+};
+
+
+export const updateBlog = (req,res)=>{
+    
+    const sql ="UPDATE blog set title = ?, description = ? WHERE id = ?";
+    const value = req.body
+    console.log(value);
+
+    db.query(sql,[value.title, value.description, value.id],(err,result)=>{
+        if(err) return res.status(500).send(err);
+        return res.status(200).send({message: "Value Updated", result})
+    })
+
+}
--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 
-import { loginUser, registerUser } from "../controllers/auth.js";
+import { createBlog, deleteBlog, getBlog, loginUser, registerUser, updateBlog } from "../controllers/auth.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post('/login', loginUser)
 authRoutes.post('/register', registerUser)
+authRoutes.post('/create', createBlog)
+authRoutes.get('/get', getBlog)
+authRoutes.delete('/delete/:id', deleteBlog)
+authRoutes.put('/update', updateBlog)
 
 export default authRoutes;
