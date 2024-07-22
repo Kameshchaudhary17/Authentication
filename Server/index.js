552e7f20a5dde0e7ef71a986565ae7dbf846c3bb
Kamesh Chaudhary <kameshchaudhary17@gmail.com>
2024-07-22 13:22:01 +0545

    chill

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,10 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:5173"],
+    credentials:true
+}));
 
 app.use('/', authRoutes)
 app.use('/', blogRoutes)
